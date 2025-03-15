b4bcdb373b144e0a424f58c8d64a3b8a1c462b2a
Arun Rengaraman <62067339+ArunRengaraman@users.noreply.github.com>
2025-03-15 18:33:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,10 @@ CLASSES = ['No Tumor', 'Benign Tumor', 'Malignant Tumor', 'Pituitary Tumor']
 
 # Load models
 MODEL_PATHS = {
-    "EfficientNet": "effnet.h5"  # Update with actual model path
+    "CNN Model": "brain_tumor_cnn.h5",  # Update with actual model path
+    "EfficientNet": "brain_tumor_efficientnet.h5"  # Update with actual model path
 }
+
 
 # Sidebar for model selection
 st.sidebar.header("Model Selection")
