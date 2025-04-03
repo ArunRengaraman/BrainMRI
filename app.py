f63f4497bbdf4bf2719644f62e33ba551a2941de
Arun Rengaraman <62067339+ArunRengaraman@users.noreply.github.com>
2025-04-03 17:50:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ CLASSES = ['No Tumor', 'Benign Tumor', 'Malignant Tumor', 'Pituitary Tumor']
 # Load models (Ensure correct paths)
 MODEL_PATHS = {
     "DensenetModel": "densenet121latest.h5",  # Update with actual model path
-    "EfficientNet": "densenet121latest.h5"  # Update with actual model path
+    "EfficientNet": "EfficientNetB0_New.h5"  # Update with actual model path
 }
 
 # Verify model file existence
